993c267973acee81be58369a0417fa24d53f0f16
shuq3 <154421897@qq.com>
2019-12-31 22:35:41 +0800

    更新博文

--- update.sh
+++ update.sh
@@ -3,15 +3,15 @@
 # 编译网站
 jekyll build
 cd _site
-rm -r shuq3.github.io
+rm -r An0nym6.github.io
 cd ..
 mv _site/archives.html _site/archives/index.html
-cd shuq3.github.io
+cd An0nym6.github.io
 rm -rf *
 cd ..
-cp -r _site/. shuq3.github.io
+cp -r _site/. An0nym6.github.io
 
 # 上传网站到 GitHub Pages
-cd shuq3.github.io
+cd An0nym6.github.io
 git add -A && git commit -m "更新博文"
 git push
